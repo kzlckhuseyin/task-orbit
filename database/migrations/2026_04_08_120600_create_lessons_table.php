8d776ea6ef83d1dbf76f92127f69564f1ee4380a
kurtemrecan <can.kurt764@gmail.com>
2026-04-08 17:04:20 +0300

    feat: migrations oluşturuldu

---
+++ database/migrations/2026_04_08_120600_create_lessons_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('lessons', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->foreignId('internship_id')->constrained()->onDelete('cascade');
+            $table->timestamp('start_date');
+            $table->timestamp('end_date');
+            $table->text('description');
+            $table->text('content');
+            $table->foreignId('profile_id')->constrained()->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('lessons');
+    }
+};
